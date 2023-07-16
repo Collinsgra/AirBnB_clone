d1534dc50e219f360184095fb3a34410bb225ae8
clncheruiyot <clncheruiyot@gmail.com>
2023-07-16 14:16:33 -0700

    amenity.py

---
+++ amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""File contain class amenity"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Its the amenity class that succeeds BaseModel class
+    """
+    name = ""
