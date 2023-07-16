4196f3097c53da72adbc3e440219cf49935977fa
clncheruiyot <clncheruiyot@gmail.com>
2023-07-16 14:14:30 -0700

    city.py

---
+++ city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""File contain class City"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    The city class succeed what is coming from BaseModel class
+    """
+    state_id = ""
+    name = ""
+
